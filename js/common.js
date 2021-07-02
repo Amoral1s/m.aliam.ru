559c96396c5e1b11175004579828f0db9632d649
Dmitriy Filippov <Amoralis.19@gmail.com>
2021-07-02 11:33:25 +0300

    1

--- js/common.js
+++ js/common.js
@@ -17,6 +17,10 @@ jQuery(document).ready(function ($) {
     $('.overlay-popup').fadeIn(200);
     $('.popup-consult').fadeIn(200);
   });
+  $('.call-franchise').on('click', function() {
+    $('.overlay-popup').fadeIn(200);
+    $('.popup-franchise').fadeIn(200);
+  });
   $('.overlay-popup').on('click', function() {
     $('.overlay-popup').fadeOut(200);
     $('.popup').fadeOut(200);
@@ -76,8 +80,52 @@ jQuery(document).ready(function ($) {
     prevArrow: '<div class="slider-arrow slick-next"></div>',
     nextArrow: '<div class="slider-arrow slick-prev"></div>'
   });
+  $('.franchise-services .services-wrap').slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    speed: 300,
+    rtl: false,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    prevArrow: '<div class="slider-arrow slick-next"></div>',
+    nextArrow: '<div class="slider-arrow slick-prev"></div>'
+  });
   
   $('.page-team-wrap-item-content').slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    speed: 300,
+    rtl: false,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    prevArrow: '<div class="slider-arrow slick-next"></div>',
+    nextArrow: '<div class="slider-arrow slick-prev"></div>'
+  });
+  $('.single-services-how-wrap').slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    speed: 300,
+    rtl: false,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    prevArrow: '<div class="slider-arrow slick-next"></div>',
+    nextArrow: '<div class="slider-arrow slick-prev"></div>'
+  });
+  $('.single-services-who-wrap').slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    speed: 300,
+    rtl: false,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    prevArrow: '<div class="slider-arrow slick-next"></div>',
+    nextArrow: '<div class="slider-arrow slick-prev"></div>'
+  });
+  $('.single-services-cost-wrap').slick({
     dots: false,
     arrows: false,
     infinite: false,
@@ -197,62 +245,9 @@ jQuery(document).ready(function ($) {
     });
   } 
 
-  $('select').addClass('select');
-
-  $('.select').each(function() {
-    const _this = $(this),
-        selectOption = _this.find('option'),
-        selectOptionLength = selectOption.length,
-        selectedOption = selectOption.filter(':selected'),
-        duration = 150; // длительность анимации 
-
-    _this.hide();
-    _this.wrap('<div class="select"></div>');
-    $('<div>', {
-        class: 'new-select',
-        text: _this.children('option:disabled').text()
-    }).insertAfter(_this);
-
-    const selectHead = _this.next('.new-select');
-    $('<div>', {
-        class: 'new-select__list'
-    }).insertAfter(selectHead);
-
-    const selectList = selectHead.next('.new-select__list');
-    
-    for (let i = 1; i < selectOptionLength; i++) {
-        $('<div>', {
-            class: 'new-select__item',
-            html: $('<span>', {
-                text: selectOption.eq(i).text()
-            })
-        })
-        .attr('data-value', selectOption.eq(i).val())
-        .appendTo(selectList);
-    }
-
-    const selectItem = selectList.find('.new-select__item');
-    selectList.slideUp(0);
-    selectHead.on('click', function() {
-        if ( !$(this).hasClass('on') ) {
-            $(this).addClass('on');
-            selectList.slideDown(duration);
-
-            selectItem.on('click', function() {
-                let chooseItem = $(this).data('value');
-
-                $('select').val(chooseItem).attr('selected', 'selected');
-                selectHead.text( $(this).find('span').text() );
-
-                selectList.slideUp(duration);
-                selectHead.removeClass('on');
-            });
-
-        } else {
-            $(this).removeClass('on');
-            selectList.slideUp(duration);
-        }
-    });
+  $('.ask textarea').on('focus', function() {
+    $('.ask input').slideDown(200);
+    $(this).css('height', '150px')
   });
 
 });
